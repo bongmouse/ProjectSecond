a42af14131c1efb95e861ce5e517b00d54f719f3
Kyungjin <tnfkraos97@naver.com>
2022-03-28 14:08:52 +0900

    second_commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import First from './component/first';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <First/>
     </div>
   );
 }
