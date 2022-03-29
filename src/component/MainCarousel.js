de2ebf21e453f8afb763637d0e28d1a0df66318c
Kyungjin <tnfkraos97@naver.com>
2022-03-29 15:49:17 +0900

    0329_1.2

--- src/component/MainCarousel.js
+++ src/component/MainCarousel.js
@@ -8,10 +8,9 @@ function MainCarousel(props) {
             <Carousel >
                 <Carousel.Item >
                         <img
-                        className="d-block w-100"
                         src="http://blog.jinbo.net/attach/615/200937431.jpg"
                         alt="First slide"
-                        class='carousel_img'/>
+                        className='carousel_img'/>
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
@@ -19,10 +18,9 @@ function MainCarousel(props) {
                 </Carousel.Item>
                     <Carousel.Item>
                         <img
-                        className="d-block w-100"
                         src="http://blog.jinbo.net/attach/615/200937431.jpg"
                         alt="Second slide"
-                        class='carousel_img'/>
+                        className='carousel_img'/>
 
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
@@ -31,10 +29,9 @@ function MainCarousel(props) {
                 </Carousel.Item>
                     <Carousel.Item>
                         <img
-                        className="d-block w-100"
                         src="http://blog.jinbo.net/attach/615/200937431.jpg"
                         alt="Third slide"
-                        class='carousel_img'/>
+                        className='carousel_img'/>
 
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
